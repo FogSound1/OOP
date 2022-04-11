5e632ffc3e2bfa3f61ea153aafa251130c88da1f
FogSound1 <89969634+FogSound1@users.noreply.github.com>
2022-04-11 15:30:22 +0300

    Update Lab 2C++.h

--- Лабораторна робота №2/Lab 2C++.h
+++ Лабораторна робота №2/Lab 2C++.h
@@ -1,5 +1,3 @@
 #pragma once
 void operation();
-void fillin(int n, int m, int size);
-void outputarr(int** overall, int n, int size);
-void minmax(int** overall, int n, int size);
+void execution(int quant, int n, int method);
